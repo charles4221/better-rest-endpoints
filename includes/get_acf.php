2718c5651e1eb11a9dd8cff3cf8c4a8a3beae4db
root <root@ws1.guerrilla.com.au>
2019-02-20 16:55:24 +1000

    Modify ACF PostObject response

--- includes/get_acf.php
+++ includes/get_acf.php
@@ -7,6 +7,19 @@
  * @since 0.0.1
  */
 
+function deep_modify_acf_post_objects( &$item, $key ) {
+	// Post Object field is the only one with the 'post_type' property.
+	if ( isset($item->post_type) ) {
+		$item = array(
+			'id' => $item->ID,
+			'slug' => $item->post_name,
+			'type' => $item->post_type,
+			'title' => $item->post_title,
+			'permalink' => get_the_permalink( $item->ID )
+		);
+	}
+}
+
 function bre_get_acf() {
   
   include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
@@ -19,7 +32,9 @@ function bre_get_acf() {
 
      // if we have fields
      if( $acf_fields ) {
-       return $acf_fields;
+	 	// Modify all post object responses.
+	 	array_walk_recursive( $acf_fields, 'deep_modify_acf_post_objects' );
+     	return $acf_fields;
      }
 
    } else {
