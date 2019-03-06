18327949a5935d987db5db3803f31b7256b2123e
Charles Harwood <charles@guerrilla.com.au>
2019-03-06 11:31:18 +1000

    Add post excerpt and featured image to post_object acf field

--- includes/get_acf.php
+++ includes/get_acf.php
@@ -9,36 +9,37 @@
 
 function deep_modify_acf_post_objects( &$item, $key ) {
 	// Post Object field is the only one with the 'post_type' property.
-	if ( isset($item->post_type) ) {
+	if ( isset( $item->post_type ) ) {
 		$item = array(
 			'id' => $item->ID,
 			'slug' => $item->post_name,
 			'type' => $item->post_type,
 			'title' => $item->post_title,
-			'permalink' => get_the_permalink( $item->ID )
+			'excerpt' => $item->post_excerpt,
+			'permalink' => get_the_permalink( $item->ID ),
+			'image' => get_the_post_thumbnail( $item, 'full' ),
 		);
 	}
 }
 
 function bre_get_acf() {
-  
-  include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-  // check if acf is active before doing anything
-   if( is_plugin_active('advanced-custom-fields-pro/acf.php') || is_plugin_active('advanced-custom-fields/acf.php') ) {
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-     // get fields
-     $acf_fields = get_fields();
+	// check if acf is active before doing anything
+	if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) || is_plugin_active( 'advanced-custom-fields/acf.php' ) ) {
 
-     // if we have fields
-     if( $acf_fields ) {
-	 	// Modify all post object responses.
-	 	array_walk_recursive( $acf_fields, 'deep_modify_acf_post_objects' );
-     	return $acf_fields;
-     }
+		// get fields
+		$acf_fields = get_fields();
 
-   } else {
-     // no acf, return false
-     return false;
-   }
+		// if we have fields
+		if ( $acf_fields ) {
+			// Modify all post object responses.
+			array_walk_recursive( $acf_fields, 'deep_modify_acf_post_objects' );
+			return $acf_fields;
+		}
+	} else {
+		  // no acf, return false
+		  return false;
+	}
 }
